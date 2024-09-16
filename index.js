14da75a32f299376f46e22aeba214d4bec2500f8
Tazz <tazz@tazz.cloud>
2024-09-15 22:56:06 -0400

    feat(verifyconditions): add code to check for CMakeLists.txt in project root

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+import SemanticReleaseError from '@semantic-release/error';
+import fs from 'fs';
 
+let verified = false;
 
 export async function verifyConditions(config, context) {
   const {
+    cwd,
     options: {},
     logger,
   } = context;
-  logger.log(`verifyConditions`);
+  const listfile = `${cwd}/CMakeLists.txt`;
+  if(!fs.existsSync(listfile))
+    throw new SemanticReleaseError(`cannot find root CMakeLists.txt`);
+  verified = true;
 }
 
 export async function publish(config, context) {
@@ -20,14 +27,22 @@ export async function success(config, context) {
   const {
     options: {},
     logger,
+    releases,
   } = context;
-  logger.log(`success`);
+  logger.log(`success, releases:`);
+  for(let release in releases) {
+    logger.log(`- ${release}`);
+  }
 }
 
 export async function fail(config, context) {
   const {
     options: {},
     logger,
+    errors,
   } = context;
-  logger.log(`fail`);
+  logger.log(`failed, errors:`);
+  for(let error in errors) {
+    logger.log(`- ${error}`);
+  }
 }
